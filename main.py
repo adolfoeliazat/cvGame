d457a86628b27ac8c584ab74e1bc673e03df401e
Bruno Macabeus <bruno.macabeus@gmail.com>
2015-10-25 19:57:05 -0300

    Improved readability on the generate game piece

--- main.py
+++ main.py
@@ -48,6 +48,27 @@ def game():
 
     place_piece_success = random.randrange(total_pieces)
 
+    def invert_piece(piece):
+        return piece[::-1]
+
+    def color_red_to_green(piece):
+        for i in piece:
+                    for i2 in i:
+                        if (i2[2] > i2[1] + 40) and (i2[2] > i2[0] + 40):
+                            i2[2], i2[1] = i2[1], i2[2]
+
+        return piece
+
+    def color_black_to_blue(piece):
+        for i in piece:
+            for i2 in i:
+                if (np.array([80, 80, 80]) > i2).all():
+                    i2[0] += 100
+
+        return piece
+
+    piece_change_functions = [invert_piece, color_red_to_green, color_black_to_blue]
+
     for current_piece in range(total_pieces):
         x = random.randrange(height - 50)
         y = random.randrange(width - 50)
@@ -55,22 +76,7 @@ def game():
         piece = copy.copy(img[x:x + 50, y:y + 50])
 
         if current_piece != place_piece_success:
-            piece_type = random.randrange(3)
-
-            if piece_type == 0:
-                piece = piece[::-1]
-
-            elif piece_type == 1:
-                for i in piece:
-                    for i2 in i:
-                        if (i2[2] > i2[1] + 40) and (i2[2] > i2[0] + 40):
-                            i2[2], i2[1] = i2[1], i2[2]
-
-            elif piece_type == 2:
-                for i in piece:
-                    for i2 in i:
-                        if (np.array([80, 80, 80]) > i2).all():
-                            i2[0] += 100
+            piece = random.choice(piece_change_functions)(piece)
 
         img_game[height + 25:height + 75, 100 * current_piece:50 + 100 * current_piece] = piece
 
